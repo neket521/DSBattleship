9e300fef02f81fd8ac3c8cf05b8179daf25c845c
Anton Prokopov <prokopov@hotmail.com>
2016-12-13 20:58:12 +0200

    Server asks and validates login, then asks to select the game

--- Client/client.py
+++ Client/client.py
@@ -1,6 +1,6 @@
 import uuid
 import pika
-from common import REQ_CONNECT, REQ_EXIT
+from common import STATUS_CONNECTED, STATUS_EXIT, STATUS_LOGIN_FAIL, STATUS_CHOOSE_GAME, STATUS_GAME_SELECTED
 
 class Client:
 
@@ -9,6 +9,9 @@ class Client:
     ___BUILT = '2016-12-13'
     ___VENDOR = 'Copyright (c) Anton Prokopov, Nikita Kirienko'
     ___EXIT_COMMAND = 'Type \'exit\' to leave the game'
+
+    login = None
+    status = -1
 
     def info(self):
         return '%s version %s (%s) %s\n%s' % (Client.___NAME, Client.___VER, Client.___BUILT, Client.___VENDOR, Client.___EXIT_COMMAND)
@@ -20,11 +23,19 @@ class Client:
         self.callback_queue = result.method.queue
         self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
 
+    def start_game(self):
+        login = raw_input('Enter user login and press Enter:\n')
+        print self.connect(login)
+        if Client.status != STATUS_LOGIN_FAIL:
+            # remember login to use it, when disconnecting
+            Client.login = login
+
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
-            self.response = body
+            Client.status = int(body.split(':')[0])
+            self.response = body.split(':')[1]
 
-    def call(self, reqCode):
+    def call(self, reqCode, body='empty_body'):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
@@ -33,27 +44,53 @@ class Client:
                                        reply_to=self.callback_queue,
                                        correlation_id=self.corr_id,
                                    ),
-                                   body=str(reqCode) + ':' + 'empty_body')
+                                   body=str(reqCode) + ':' + body)
         while self.response is None:
             self.connection.process_data_events()
         return self.response
 
-    def connect(self):
-        connect_request = REQ_CONNECT
-        return self.call(connect_request)
+    def connect(self, login):
+        request_code = STATUS_CONNECTED
+        return self.call(request_code, login)
 
-    def exit_game(self):
-        exit_request = REQ_EXIT
-        return self.call(exit_request)
+    def exit_game(self, login):
+        request_code = STATUS_EXIT
+        if login != None:
+            return self.call(request_code, login)
+        else:
+            return None
+
+    def get_list_of_active_games(self):
+        request_code = STATUS_CHOOSE_GAME
+        return self.call(request_code)
+
+    def select_game(self, n):
+        request_code = STATUS_GAME_SELECTED
+        return self.call(request_code, n)
+
+    def loop(self):
+        try:
+            print 'Falling to loop...'
+            while 1:
+                if Client.status == STATUS_LOGIN_FAIL:
+                    self.start_game()
+                    continue
+                if Client.status == STATUS_CONNECTED:
+                    print 'Choose a game to join from the list below:'
+                    print client.get_list_of_active_games()
+                    selected_game = raw_input('Type the number and press Enter:\n')
+                    print self.select_game(selected_game)
+                    continue
+                if Client.status == STATUS_EXIT:
+                    print 'You have been kicked'
+                    break
+                if raw_input() == 'exit':
+                    print self.exit_game(Client.login)
+                    break
+        except KeyboardInterrupt:
+            print self.exit_game(Client.login)
 
 client = Client("127.0.0.1")
 print client.info()
-print client.connect()
-try:
-    print 'Falling to loop...'
-    while 1:
-        if raw_input() == 'exit':
-            print client.exit_game()
-            break
-except KeyboardInterrupt:
-    print client.exit_game()
+client.start_game()
+client.loop()
--- Server/server.py
+++ Server/server.py
@@ -1,27 +1,59 @@
 import pika
-from common import REQ_CONNECT, REQ_EXIT
+from common import STATUS_CONNECTED, STATUS_EXIT, STATUS_LOGIN_FAIL, STATUS_CHOOSE_GAME, STATUS_GAME_SELECTED, \
+    STATUS_POSITION_SHIPS
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
     host='127.0.0.1', port = 5672))
 channel = connection.channel()
 channel.queue_declare(queue='rpc_queue')
 
+connected_players = []
 
 class Player:
+
     def __init__(self, login):
-        self.login = login
+        self.login = str(login)
         self.score = 0
 
+    def get_login(self):
+        return self.login
+
+def prepare_list_of_active_games():
+    return '0. Host your game\n1. bob\'s game'
+
+def prepare_response(reqCode, request):
+    #print str(reqCode)
+    if reqCode == STATUS_CONNECTED:
+        for i in range(len(connected_players)):
+            if request == connected_players[i].get_login():
+                return str(STATUS_LOGIN_FAIL)+':'+'This login already in use, try another one'
+        connected_players.append(Player(request))
+        #check if there are available games
+        return str(STATUS_CONNECTED)+':'+'Connected as '+request
+    elif reqCode == STATUS_EXIT:
+        for i in range(len(connected_players)):
+            if request == connected_players[i].get_login():
+                connected_players.pop(i)
+        return str(STATUS_EXIT)+':'+'Exiting the game'
+    elif reqCode == STATUS_CHOOSE_GAME:
+        return str(STATUS_CHOOSE_GAME)+':'+prepare_list_of_active_games()
+    elif reqCode == STATUS_GAME_SELECTED:
+        try:
+            selected_game = int(request)
+            if selected_game == 0:
+                msg = 'You have hosted the game'
+            else:
+                msg = 'You have joined the game'
+        except:
+            return str(STATUS_CONNECTED)+':'+'Wrong value is entered'
+        return str(STATUS_GAME_SELECTED)+':'+msg
+    else:
+        return None
 
 def on_request(ch, method, props, body):
     reqCode = int(body.split(':')[0])
     request = body.split(':')[1]
-    response = None
-    if reqCode == REQ_CONNECT:
-        response = 'Connected'
-    elif reqCode == REQ_EXIT:
-        response = "Exiting the game"
-    print("on_request")
+    response = prepare_response(reqCode, request)
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = props.correlation_id,),
