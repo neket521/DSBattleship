3787f1d1ad21bf21de5e404ff04646a8665beea0
Anton Prokopov <prokopov@hotmail.com>
2016-12-13 22:45:02 +0200

    A few improvements in creating a game field (board)

--- UI/board.py
+++ UI/board.py
@@ -1,17 +1,21 @@
 import sys
 import random
+from common import FIELD_EMPTY, FIELD_SHIP
+
 class Board():
+
     def __init__(self):
-        self.size = 10
+        self.size_to_print = 10
+        self.size = 11
+        self.info = 'Ships to position:\nCarrier of size 4 x1, Battleship of size 3 x2, Cruiser of size 2 x3, Submarine of size 1 x4'
 
     def add_ships(self):
         ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
         types = ["Carrier: 4", "Battleship: 3", "Cruiser: 2", "Submarine: 1"]
         while 1:
-            print"Do you want to place ships manually(m), or allow AI(a) to place them randomly?"
-            answer = raw_input()
+            print self.info
+            answer = raw_input("Do you want to place ships manually(m), or allow AI(a) to place them randomly?\n")
             if answer == 'a':
-                self.size = 11
                 self.list = [[0 for x in range(self.size)] for x in range(self.size)]
                 for e in ships:
                     x = random.randint(0, self.size)
@@ -19,29 +23,26 @@ class Board():
                     d = random.randint(0, 1)
                     self.add_ship(e,x,y,d)
                 map(lambda x: x.pop(), self.list).pop()
-                self.size = 10
                 break
             if answer == 'm':
-                self.size = 11
                 self.list = [[0 for x in range(self.size)] for x in range(self.size)]
-                for e in ships:
-                    letters = 'ABCDEFGHIJ'
-                    while 1:
-                        x = raw_input('Enter ' + str(e) + ' sized ship coordinates: letter and number(for example A1) ')
-                        d = raw_input('Enter ' + str(e) + ' sized ship direction(0 = vertical,1 = horisontal) ')
-                        if x != '' and d != '':
-                            if x[0].lower() in letters.lower() and 0 <= int(d) <= 1:
-                                break
-                    self.size = 11
-                    y = letters.lower().index(x[0].lower())
-                    self.place_manually(e,int(x[1])-1,y,int(d))
-                    #print self.list
-                    self.size = 10
+                self.print_board()
+                for ship in ships:
+                    self.ask_coordinates_and_place_ship(ship)
                     self.print_board()
-
                 map(lambda x: x.pop(), self.list).pop()
-                print
                 break
+
+    def ask_coordinates_and_place_ship(self, ship):
+        letters = 'ABCDEFGHIJ'
+        while 1:
+            x = raw_input('Enter ' + str(ship) + ' sized ship coordinates: letter and number(for example A1):\n')
+            d = raw_input('Enter ' + str(ship) + ' sized ship direction(0 = vertical,1 = horizontal):\n')
+            if x != '' and d != '':
+                if x[0].lower() in letters.lower() and 0 <= int(d) <= 1:
+                    break
+        y = letters.lower().index(x[0].lower())
+        self.place_manually(ship, int(x[1]) - 1, y, int(d))
 
     def place_manually(self,ship,x,y,d):
         if self.place_available(ship, x, y, d):
@@ -56,14 +57,7 @@ class Board():
                     y += 1
         else:
             print "Not available place"
-            while 1:
-                x = raw_input('Enter ' + str(ship) + ' sized ship x coordinate ')
-                y = raw_input('Enter ' + str(ship) + ' sized ship y coordinate ')
-                d = raw_input('Enter ' + str(ship) + ' sized ship direction(0 = vertical,1 = horizontal) ')
-                if x != '' and y != '' and d != '':
-                            if 0 <= int(x) <= 9 and  0 <= int(y) <= 9 and 0 <= int(d) <= 1:
-                                break
-            self.place_manually(ship,int(y),int(x),int(d))
+            self.ask_coordinates_and_place_ship(ship)
 
     def add_ship(self,ship,x,y,d):
         if self.place_available(ship, x, y, d):
@@ -83,16 +77,16 @@ class Board():
 
     def print_board(self):
         print "    A  B  C  D  E  F  G  H  I  J"
-        for i in range(self.size):
+        for i in range(self.size_to_print):
             if i == 9:
                 sys.stdout.write(" " + str(i+1))
             else:
                 sys.stdout.write(" " + str(i+1) + ' ')
-            for j in range(self.size):
+            for j in range(self.size_to_print):
                 if self.list[i][j] != 0:
-                    sys.stdout.write(" " + str(self.list[i][j])+ " ")
+                    sys.stdout.write(FIELD_SHIP)
                 else:
-                    sys.stdout.write(" 0 ")
+                    sys.stdout.write(FIELD_EMPTY)
             print
 
     def place_available(self,size,x,y,d):
